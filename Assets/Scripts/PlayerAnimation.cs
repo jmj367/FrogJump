50b6db7dd839e7a12e8e4748425015ebb97cb99d
jmj367 <wakazu1106@gmail.com>
2024-11-06 09:51:56 +0900

    PlayerAnimation準備

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -1,17 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 前フレームのisJumpStart
+    /// </summary>
+    private bool isBeforeJump = false;
+
+    //入力値
+    /// <summary>
+    /// ジャンプボタンが押されたか
+    /// </summary>
+    private bool isJumpStart = false;
+
+    /// <summary>
+    /// プレイヤーの状態
+    /// </summary>
+    private enum State
+    {
+        Idle,
+        Jump,
+    }
+
+    /// <summary>
+    /// プレイヤーの現在のステータス
+    /// </summary>
+    private State currentState = State.Idle;
+
+    //ジャンプ開始入力受取
+    public void OnJumpStart(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started)
+        {
+            isJumpStart = true;
+        }
+
+        if(context.phase == InputActionPhase.Canceled)
+        {
+            isJumpStart = false;
+        }
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Idle状態の更新
+    /// </summary>
+    private void UpdateIdle()
     {
         
     }
